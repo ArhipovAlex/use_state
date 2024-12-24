18ffe72ac0406e78b01024fe0b1a9e763911c9cd
Александр Архипов <liney13@mail.ru>
2024-12-24 19:25:02 +0800

    TaskList DONE

--- src/tasklist/Task.js
+++ src/tasklist/Task.js
@@ -2,9 +2,15 @@ function Task(props)
 {
     return(
         <div className="task">
+        <div  style={{textDecoration: props.task.done ? "line-through": ""}}>
             {props.task.text}
-            <button>DONE</button>
         </div>
+<div>
+            <button onClick={()=>props.doneTask(props.index)}>DONE</button>
+            <button onClick={()=>props.deleteTask(props.index)}>X</button>
+</div>
+        </div>
+
     )
 }
 export default Task;
--- src/tasklist/TaskList.js
+++ src/tasklist/TaskList.js
@@ -31,12 +31,24 @@ function TaskList()
         let newTask =[...tasks,{text}];
         setTasks(newTask);
     }
+    let doneTask = index => 
+        {
+            let newTask = [...tasks];
+            newTask[index].done = !newTask[index].done;
+            setTasks(newTask);
+        }
+    let deleteTask = index =>
+    {
+        let newTask = [...tasks];
+        newTask.splice(index,1);
+        setTasks(newTask);
+    }
     return(
         <div className="task-list">
             {
                 tasks.map(
                     (task, index) => (
-                        <Task key={index} task={task}/>
+                        <Task key={index} task={task} index={index} doneTask={doneTask} deleteTask={deleteTask}/>
                     )
                 )
             }
